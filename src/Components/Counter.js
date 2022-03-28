461237cf49b6e28b011338ce077960fb59ba7eaa
anuup <anupb44@gmail.com>
2022-03-28 11:37:56 +0530

    new redux project

---
+++ src/Components/Counter.js
@@ -0,0 +1,22 @@
+import {useState} from 'react'
+function Counter() {
+  const [count, setCount] = useState(4)
+  const increment=()=>{
+setCount(count+1)
+
+  }
+  const decrement=()=>{
+    setCount(count-1)
+    
+      }
+  return (
+    <div className="counter">
+      <h2>Counter</h2>
+      <h3>The count is - {count}</h3>
+      <button onClick={increment}>Increase</button>
+      <button onClick={decrement}>Decrease</button>
+    </div>
+  );
+}
+
+export default Counter;
